a9d7fb5387c0ae3701af554de20ef25c95b8b5b6
BidhuMRenchi <bidhu.renchi@experionglobal.com>
2022-02-12 16:56:37 +0530

    COMMIT 002 by BIDHU M RENCHI

--- src/app/customers/customer/customer.component.ts
+++ src/app/customers/customer/customer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CustomersService } from 'src/app/shared/customers.service';
 
 @Component({
   selector: 'app-customer',
@@ -7,9 +11,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
+  userId:number=0;
+  constructor(
+    public userService:CustomersService,
+    private route: ActivatedRoute,
+    private toastrService:ToastrService
+  ) { }
 
   ngOnInit(): void {
-  }
 
+    //get userId from Activate route
+    this.userId = this.route.snapshot.params['CId'];
+    if (this.userId != 0 || this.userId != null) {
+      this.userService.getUserById(this.userId).subscribe(
+        result => {
+          console.log("Retrieving get By Id");
+          console.log(result);
+
+          // //format the date : yyyy-MM-DD
+          // var datePipe = new DatePipe("en-UK");
+          // let formatedDate: any = datePipe.transform(result.Doj, 'yyyy-MM-dd')
+          // result.Doj = formatedDate;
+
+          this.userService.formData = Object.assign({}, result);
+
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }//end ngOnInIt
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+
+    //insert/UPDATE
+    let addId = this.userService.formData.CId;
+    if (addId == 0 || addId == null) {//INSERT
+      console.log("Inserting 001");
+      this.insertUserRecord(form);
+    }
+    else {
+      this.updateUserRecord(form);
+    }
+  }//end OnSubmit
+
+  //insert MEthod
+  insertUserRecord(form?: NgForm) {
+    console.log("Inserting a rercord");
+    this.userService.insertUser(form.value).subscribe(
+      (result) => {
+        console.log(result);
+        this.toastrService.success('User Record has been Inserted','DATING APP');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }//End Insert Method
+
+  //Update
+  updateUserRecord(form?: NgForm) {
+    console.log("Updating a record");
+    this.userService.updateUser(form.value).subscribe(
+      (result) => {
+        console.log(result);
+        console.log("Updated the record");
+        this.toastrService.success('User Record has been Updated','DATING APP');
+        this.resetForm(form);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }//end Update 
+
+  //clear all contents after submit -- Intialization
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
+  }//end ResetForm
 }
--- src/app/customers/customerlist/customerlist.component.ts
+++ src/app/customers/customerlist/customerlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CustomersService } from 'src/app/shared/customers.service';
 
 @Component({
   selector: 'app-customerlist',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerlistComponent implements OnInit {
 
-  constructor() { }
+ //declare varaible
+ page:number=1;
+ filter:string;
+
+ constructor(
+   public userService:CustomersService,
+   private router:Router
+ ) { }
 
   ngOnInit(): void {
+    console.log("Welcome to Life Cycle Hook");
+    this.userService.bindListUsers();
+  }
+
+  //Edit User
+  updateUser(cId : number){
+    console.log("Editing : "+cId);
+    // navigate to edit form
+    this.router.navigate(['customer',cId])
+  }
+
+  //Delete User
+  deleteUser(cId : number){
+    console.log("Deleting : "+cId);
+    if(confirm('Are you sure to delete this record ? ')){
+      this.userService.deleteUser(cId).subscribe(
+        response=>{
+          this.userService.bindListUsers();
+        },
+        error=>{
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
